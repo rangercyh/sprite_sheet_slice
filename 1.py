2666b1c5220a4323d2335b0f47dc4a0cf09a1c93
caiyiheng <rangercyh@qq.com>
2023-06-06 11:40:28 +0800

    'middle

--- 1.py
+++ 1.py
@@ -1,29 +1,36 @@
 import cv2
+import numpy as np
 
-image = cv2.imread('1.png')
+image = cv2.imread('1.png', -1)
 original = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-# close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
-# dilate = cv2.dilate(close, kernel, iterations=1)
-
 cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
 cv2.drawContours(original, cnts[0], -1, (0,.0, 255), 3, lineType=cv2.LINE_AA)
+
+max_width = 0
+max_height = 0
+for c in cnts[0]:
+    x,y,w,h = cv2.boundingRect(c)
+    if w > max_width:
+        max_width = w
+    if h > max_height:
+        max_height = h
 
 sprite_number = 0
 for c in cnts[0]:
     x,y,w,h = cv2.boundingRect(c)
     ROI = image[y:y+h, x:x+w]
-    cv2.imwrite('sprite_{}.png'.format(sprite_number), ROI)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+    bg = np.zeros((max_height, max_width, 4), np.uint8)
+    x_offset = (max_width - w) // 2
+    y_offset = (max_height - h) // 2
+    bg[y_offset:y_offset+h, x_offset:x_offset+w, :4] = ROI
+    cv2.imwrite('sprite_{}.png'.format(sprite_number), bg)
     sprite_number += 1
 
 cv2.imshow('thresh', thresh)
-# cv2.imshow('dilate', dilate)
 cv2.imshow('contours', original)
 cv2.waitKey()
 cv2.destroyAllWindows()
